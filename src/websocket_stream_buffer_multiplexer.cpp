52246f33a95d84db89a60c83220735e3a9c057b7
kzhdev <kzhdev@gmail.com>
2026-09-12 21:04:40 -0500

    Upgrade stream-buffer-multiplexer to 2.0.0; upgrade slick-dynamic-buffer to 1.0.2

--- src/websocket_stream_buffer_multiplexer.cpp
+++ src/websocket_stream_buffer_multiplexer.cpp
@@ -16,4 +16,23 @@ template Websocket<slick::dynamic_buffer<producer_buffer>>::Websocket(
     std::shared_ptr<producer_buffer>,
     uint32_t);
 
+struct net_queue_traits : slick::queue_traits {
+    static constexpr bool enable_read_last = false;
+    static constexpr bool enable_cpu_relax = false;
+};
+
+using buffer_multiplexer = slick::basic_stream_buffer_multiplexer<net_queue_traits>;
+using fast_producer_buffer = buffer_multiplexer::producer_buffer;
+
+template class Websocket<slick::dynamic_buffer<fast_producer_buffer>>;
+template Websocket<slick::dynamic_buffer<fast_producer_buffer>>::Websocket(
+    std::string,
+    std::function<void()> &&,
+    std::function<void()> &&,
+    std::function<void(const char*, std::size_t)> &&,
+    std::function<void(std::string &&err)> &&,
+    std::shared_ptr<fast_producer_buffer>,
+    uint32_t);
+
+
 } // namespace slick::net
